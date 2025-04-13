019ff33ea61e25ff57ed9056fb03831ae721243c
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2025-04-13 13:28:37 +0100

    initial commit

---
+++ src/main/java/com/gaming/gaminglegensbackend/GamingLegensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaming.gaminglegensbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingLegensBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingLegensBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaming/gaminglegensbackend/GamingLegensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaming.gaminglegensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingLegensBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
